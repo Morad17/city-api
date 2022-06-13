4e04496438f69c31769eb099a101125ac46a6c19
Morad17 <morad123@hotmail.co.uk>
2022-06-13 23:07:13 +0100

    basic outline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
+import Home from "./pages/Home";
 
 
 function App() {
-  return (
-    <div className="App">
-     My App
-    </div>
-  );
+  return <Home />
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="main-nav">
+      <ul>
+        <li className="nav-link">link</li>
+        <li className="nav-link">link</li>
+        <li className="nav-link">link</li>
+        <li className="nav-link">link</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
